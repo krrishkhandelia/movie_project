6692aad4bfd6ff60dcc6f46105610a283ad34117
Krrish Khandelia <krrish_ug_24@ece.nits.ac.in>
2025-03-11 19:58:59 +0530

    deleted gand.png

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import MovieCard from './components/MovieCard'
 
+
 function App() {
   return (
   <div>
     <MovieCard movie={{title: "krrish ki picture", release_date:2006}} />
-    <MovieCard movie={{url: './gand.png', title: "ansh ki picture", release_date:2004}} />
+    <MovieCard movie={{title: "ansh ki picture", release_date:2004}} />
   </div>
   )
 }
