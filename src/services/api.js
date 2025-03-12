2ab4986bbde5dfdd8e8533b056334e29d2776365
Krrish Khandelia <krrish_ug_24@ece.nits.ac.in>
2025-03-12 20:15:59 +0530

    added css and loaded the API

---
+++ src/services/api.js
@@ -0,0 +1,17 @@
+const API_KEY = "bee0c0b013ec9373fc39e15140ff7b7c";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
